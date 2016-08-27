852ec56c040f84e6fff04ddb3ceffc902a29b27b
wiatrogon <wiatrogon@gmail.com>
2016-08-27 15:11:00 +0200

    - things are moving now

--- create.js
+++ create.js
@@ -1,4 +1,6 @@
 var player;
+var metalface;
+var monster1, monster2;
 var platforms;
 var cursors;
 var jumpButton;
@@ -7,30 +9,41 @@ function create() {
 
     player = game.add.sprite(100, 200, 'player');
 
-    monster1 = game.add.sprite(700, 100, 'monster');
-    monster2 = game.add.sprite(700, 400, 'monster');
+    function add_monster (x, y) {
+        monster = game.add.sprite(x, y, 'monster');
+        monster.anchor.setTo(.5, 1);
+        monster.animations.add('walk');
+        monster.animations.play('walk', 15, true);
+        game.physics.arcade.enable(monster);
+        monster.body.collideWorldBounds = true;
+        monster.body.gravity.y = 500;
+        return monster;
+    }
+
+    monster1 = add_monster(700, 100);
+    monster1.body.velocity.x = 50;
+    monster2 = add_monster(700, 400);
+    monster2.body.velocity.x = 50;
 
     game.physics.arcade.enable(player);
-    game.physics.arcade.enable(monster1);
-    game.physics.arcade.enable(monster2);
 
     player.body.collideWorldBounds = true;
     player.body.gravity.y = 500;
-
-    monster1.body.collideWorldBounds = true;
-    monster1.body.gravity.y = 500;
-
-    monster2.body.collideWorldBounds = true;
-    monster2.body.gravity.y = 500;
 
     platforms = game.add.physicsGroup();
 
     platforms.create(500, 150, 'platform');
     platforms.create(-200, 300, 'platform');
     platforms.create(400, 450, 'platform');
-    platforms.create(100, 100, 'metalface');
+
+    metalface = game.add.sprite(310, 100, 'metalface');
+    blink = metalface.animations.add('blink');
+    metalface.animations.play('blink', 1, false);
+    platforms.add(metalface);
 
     platforms.setAll('body.immovable', true);
+
+    metalface.body.velocity.y = 50;
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
--- update.js
+++ update.js
@@ -20,4 +20,28 @@ function update () {
         player.body.velocity.y = -400;
     }
 
+    if (metalface.bottom > 500) {
+        metalface.body.velocity.y = -50;
+    }
+    else if (metalface.top < 100) {
+        metalface.body.velocity.y = 50;
+    }
+
+    if (monster1.left < 600) {
+        monster1.body.velocity.x = 50;
+        monster1.scale.x = 1;
+    }
+    else if (monster1.right > 700) {
+        monster1.body.velocity.x = -50;
+        monster1.scale.x = -1;
+    }
+
+    if (monster2.left < 500) {
+        monster2.body.velocity.x = 50;
+        monster2.scale.x = 1;
+    }
+    else if (monster2.right > 750) {
+        monster2.body.velocity.x = -50;
+        monster2.scale.x = -1;
+    }
 }
