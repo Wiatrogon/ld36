cc97c559e0e99c691317d1ac310fc9dff3a4c553
wiatrogon <wiatrogon@gmail.com>
2016-08-27 16:37:26 +0200

    - simpler velocity management?

--- update.js
+++ update.js
@@ -19,28 +19,20 @@ function update () {
         player.body.velocity.y = -400;
     }
 
-    if (metalface.bottom > 500) {
-        metalface.body.velocity.y = -50;
-    }
-    else if (metalface.top < 100) {
-        metalface.body.velocity.y = 50;
+    if (metalface.bottom > 500 && metalface.body.velocity.y > 0
+        || metalface.top < 100 && metalface.body.velocity.y < 0) {
+        metalface.body.velocity.y *= -1;
     }
 
-    if (monster1.left < 600) {
-        monster1.body.velocity.x = 50;
-        monster1.scale.x = 1;
-    }
-    else if (monster1.right > 700) {
-        monster1.body.velocity.x = -50;
-        monster1.scale.x = -1;
+    if (monster1.left < 600 && monster1.body.velocity.x < 0
+        || monster1.right > 700 && monster1.body.velocity.x > 0) {
+        monster1.body.velocity.x *= -1;
+        monster1.scale.x *= -1;
     }
 
-    if (monster2.left < 500) {
-        monster2.body.velocity.x = 50;
-        monster2.scale.x = 1;
-    }
-    else if (monster2.right > 750) {
-        monster2.body.velocity.x = -50;
-        monster2.scale.x = -1;
+    if (monster2.left < 500 && monster2.body.velocity.x < 0
+        || monster2.right > 750 && monster2.body.velocity.x > 0) {
+        monster2.body.velocity.x *= -1;
+        monster2.scale.x *= -1;
     }
 }
